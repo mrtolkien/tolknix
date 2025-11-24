c97d924b1f25a681470dabf889a404539116c7f0
mrtolkien <gary.mialaret@gmail.com>
2025-11-24 19:45:50 +0900

    feat: add gearlever + waybar to my conf

--- linux/packages.nix
+++ linux/packages.nix
@@ -4,7 +4,9 @@
 with pkgs; [
   # Home manager itself (so the command is always available)
   home-manager
+  waybar
+  gearlever # AppImage manager
+  # TUI managers
   bluetui
   pulsemixer
-  waybar
 ]
